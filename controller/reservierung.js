8a9194a9a3dc465db7e0c167e3d3af4ae5b89e31
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-17 11:51:08 +0200

    bE fetch alleReservierungen als Zahl

--- controller/reservierung.js
+++ controller/reservierung.js
@@ -9,7 +9,7 @@ export const postReservierung = async (req, res) => {
         const reservierung = await db.collection(COL).insertOne(req.body)
         console.log(reservierung)
         res.status(294).json({message: `Reservierung wurde hinzugefügt: ${reservierung}`})
-    } catch (error) {
+    } catch (err) {
         console.log(err)
         res.status(594).json( { message: `Fehler bei postReservierung: ${err}` } )
     
@@ -18,5 +18,17 @@ export const postReservierung = async (req, res) => {
 
 
 
-// alleReservierungen
-export const getAlleReservierungen = async (req, res )
+// alleReservierungen      findet Zahl  
+export const getAlleReservierungen = async (req, res ) => {
+    try {
+        const db = await getDb()
+        const reservierung = await db.collection(COL).find().count()
+        console.log(reservierung)
+        res.status(291).json( { reservierung})
+    } catch (err) {
+        console.log(err)
+        res.status(591).json( { message: `Fehler bei getAlleReservierungen: ${err}`})
+    }
+}
+
+
