fbda151c7e99108d44246044f918b60a5e16c774
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-19 10:25:07 +0200

    testing Phase FrontE

--- controller/frontEnd.js
+++ controller/frontEnd.js
@@ -4,14 +4,16 @@
 export const getFetchFrontEndAufweckenInterval = async (req, res) => {
    const aufwecken = async () => {
         try {
-          const response = await  fetch(`https://bootsverleih-frontend.onrender.com/alle-reservierungen`)
+          const response = await  fetch(`https://bootsverleih-frontend.onrender.com`)
           console.log(response)
+          res.status(247).json( { message: `FrontEnd aufgeweckt ${response}`})
             
         } catch (err) {
             console.log(err)
+            res.status(547).json( { message: `Fehler bei getFetchFrontEndAufweckenInterval: ${err} `})
         }
     }
 
-    setInterval(aufwecken(), 1200000) // 600000 = 10 Minuten 1200000 = 20 Minuten
+    setInterval(( ) => aufwecken(), 1200000) // 600000 = 10 Minuten 1200000 = 20 Minuten
 
 }
